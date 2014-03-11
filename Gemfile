48616a399c8c8fde5f8c47c8124c4dde4b1040ee
yan926 <yan926@gmail.com>
2014-03-12 02:14:54 +0800

    Finish bootstrap layout

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
+gem 'bootstrap-sass', '3.1.1'
 
 group :development, :test do
   gem 'rspec-rails'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title ='國立政治大學外國語文學院翻譯中心'
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
