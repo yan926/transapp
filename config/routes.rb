6619998eca75e1b13ae43a2ed3cf5ddc65c31802
yan926 <yan926@gmail.com>
2014-03-12 03:32:40 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Transapp::Application.routes.draw do
-  get "static_pages/home"
+  resources :users
+  root to: 'static_pages#home'
+  match '/signup',  to: 'users#new',  via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
