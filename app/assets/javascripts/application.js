9ae98f2a2b661077af60868f7a805730e905819d
yan926 <yan926@gmail.com>
2014-03-22 00:45:37 +0800

    start transweb-style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 // note: comment this > require ckeditor/init
+// del bootstrap
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
 //= require turbolinks
 //= require_tree .
